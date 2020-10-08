74d27156272e3045cfd5276a15702e2b4a289d9f
AtimaB <nickzehn10@hotmail.com>
2020-10-08 14:28:51 -0600

    Updates

---
+++ precache-manifest.aaf7098e428e01bd36b08097a18ac72a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bce397472cae989fdf80203aa16bab",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "692a082576acfc213d82",
+    "url": "/Employee-Directory/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "74364c1d1647acc28cef",
+    "url": "/Employee-Directory/static/css/main.cedfc957.chunk.css"
+  },
+  {
+    "revision": "692a082576acfc213d82",
+    "url": "/Employee-Directory/static/js/2.62e2dc9b.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/Employee-Directory/static/js/2.62e2dc9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74364c1d1647acc28cef",
+    "url": "/Employee-Directory/static/js/main.5da5d24b.chunk.js"
+  },
+  {
+    "revision": "e3aebf9144e7762f054e",
+    "url": "/Employee-Directory/static/js/runtime-main.10708a68.js"
+  },
+  {
+    "revision": "41d62814ca1dadddfc751e79bfcef6d1",
+    "url": "/Employee-Directory/static/media/buildings.41d62814.jpg"
+  }
+]);
